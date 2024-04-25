f405a362f83b8eb246bbcabb274ff1aba2143a66
ks-rjjones <rjjones@keysysconsulting.com>
2024-04-24 22:16:20 -0500

    add search, fix padding for input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -43,15 +43,36 @@ export default function Input(p: IInputProps) {
   const [isFocused, setIsFocused] = useState(false);
 
   const searchFont = useMemo(() => {
-    return `${p.helperText && p.type === "search" && "placeholder:italic placeholder:text-slate-400"}`;
+    return `${p.helperText && p.type === "search" && "placeholder:italic placeholder:text-stone-400"}`;
   }, [p.helperText, p.type]);
 
+  const SearchIcon = () => {
+    return (
+      <div className="mr-2 flex items-center">
+        <svg
+          className="size-5"
+          aria-labelledby="title desc"
+          role="img"
+          xmlns=""
+          viewBox="0 0 19.9 19.7"
+        >
+          <title id="title">Search Icon</title>
+          <desc id="desc">A magnifying glass icon.</desc>
+          <g className="stroke-stone-600" fill="none">
+            <path d="M18.5 18.3l-5.4-5.4" />
+            <circle cx="8" cy="8" r="7" />
+          </g>
+        </svg>
+      </div>
+    );
+  };
+
   const searchPadding = useMemo(() => {
-    return `${p.noFloat || p.type === "search" ? "py-1" : "pt-2"}`;
+    return `${p.noFloat || p.type === "search" ? "py-1.5" : "pt-4"}`;
   }, [p.noFloat, p.type]);
 
   const labelPos = useMemo(() => {
-    return `${p.value || isFocused ? "translate-y-0" : "translate-y-2"}`;
+    return `${p.value || isFocused ? "translate-y-0.5" : "translate-y-3"}`;
   }, [p.value, isFocused]);
 
   const labelSize = useMemo(() => {
@@ -69,25 +90,28 @@ export default function Input(p: IInputProps) {
       <div className="relative flex flex-col items-start">
         <label
           htmlFor={inputId}
-          className={`absolute top-0 w-full ${labelPos} select-none text-start ${labelSize} ${labelVisibility} leading-none text-slate-400 transition-transform`}
+          className={`absolute top-0 w-full ${labelPos} select-none text-start ${labelSize} ${labelVisibility} leading-none text-stone-400 transition-transform`}
         >
           {p.label}
         </label>
-        <input
-          id={inputId}
-          type={p.type as string}
-          value={p.value}
-          placeholder={p.noFloat || p.type === "search" ? p.label : ""}
-          className={`my-0 w-full ${searchPadding} outline-none ${searchFont}`}
-          onFocus={() => setIsFocused(true)}
-          onBlur={() => setIsFocused(false)}
-          onChange={(e) => {
-            if (p.onChange) {
-              // TODO: Logic for modify the value depending on the type.
-              p.onChange(e.target.value);
-            }
-          }}
-        />
+        <div className="flex w-full">
+          {p.type === "search" && <SearchIcon />}
+          <input
+            id={inputId}
+            type={p.type as string}
+            value={p.value}
+            placeholder={p.noFloat || p.type === "search" ? p.label : ""}
+            className={`my-0 w-full ${searchPadding} outline-none ${searchFont}`}
+            onFocus={() => setIsFocused(true)}
+            onBlur={() => setIsFocused(false)}
+            onChange={(e) => {
+              if (p.onChange) {
+                // TODO: Logic for modify the value depending on the type.
+                p.onChange(e.target.value);
+              }
+            }}
+          />
+        </div>
       </div>
       {!p.border && <hr />}
       {/* TODO: Handle error state and text */}
