49c30c49873d0b99c10d04a84a75e315d186d3c8
ks-rjjones <rjjones@keysysconsulting.com>
2024-05-16 21:35:15 -0500

    update checkbox styling

--- src/components/checkbox.tsx
+++ src/components/checkbox.tsx
@@ -1,5 +1,6 @@
-/* eslint-disable tailwindcss/no-unnecessary-arbitrary-value */ //linting bug
-import { useMemo, useState } from "react";
+// This lint ignore is to allow negative values (e.g. after:checked:translate-y-[-50%])
+/* eslint-disable tailwindcss/no-unnecessary-arbitrary-value */
+import { useEffect, useMemo, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 export interface ICheckboxProps {
@@ -15,30 +16,34 @@ export default function Checkbox(p: ICheckboxProps) {
     return `checkbox-${uuidv4()}`;
   }, []);
 
-  const [isFocused, setIsFocused] = useState(false);
   const [isHovered, setIsHovered] = useState(false);
+
+  // Note: Allow the component to control the value so we can use
+  // a div to capture the onClick event (bigger click area).
+  const [isChecked, setIsChecked] = useState(p.checked);
+  useEffect(() => {
+    p.onChange && p.onChange(isChecked);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isChecked]);
 
   return (
     <div
       onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => {
-        setIsHovered(false);
-        setIsFocused(false);
+      onMouseLeave={() => setIsHovered(false)}
+      onBlur={() => setIsHovered(false)}
+      onClick={(e) => {
+        e.preventDefault();
+        setIsChecked(!isChecked);
       }}
-      onFocus={() => setIsFocused(true)}
-      onBlur={() => {
-        setIsFocused(false);
-        setIsHovered(false);
-      }}
-      className={`w-fit items-start rounded p-2 ${p.border ? `border-2 ${isHovered ? "border-tertiary-600" : "border-tertiary-500"} ${isFocused ? "!border-primary" : ""}` : "border-2 border-transparent"}`}
+      className={`w-fit items-start rounded p-2 ${p.border ? `border-2 ${isHovered ? "border-tertiary-600" : "border-tertiary-500"}` : "border-2 border-transparent"}`}
     >
       <div className="flex items-center">
         <input
           id={checkboxId}
           type="checkbox"
-          checked={p.checked}
+          checked={isChecked}
           className={`relative block size-5 appearance-none rounded border-0 checked:!border-primary checked:!bg-primary after:checked:absolute after:checked:left-1/2 after:checked:top-[40%] after:checked:h-3/5 after:checked:w-[30%] after:checked:translate-x-[-45%] after:checked:translate-y-[-50%] after:checked:rotate-45 after:checked:border-b-2 after:checked:border-r-2 after:checked:border-tertiary-800 ${isHovered ? "bg-tertiary-500" : "bg-tertiary-400"}`}
-          onChange={(e) => p.onChange && p.onChange(e.target.checked)}
+          onChange={(e) => setIsChecked(e.target.checked)}
         />
         <label
           htmlFor={checkboxId}
