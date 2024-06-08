327dec68f5306d9a69d668df9f702ba71536b3b6
ks-rjjones <rjjones@keysysconsulting.com>
2024-06-07 22:37:23 -0500

    update form

--- src/pages/formPage.tsx
+++ src/pages/formPage.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useForm } from "react-hook-form";
 import Button from "src/components/button";
 import Input from "src/components/input";
@@ -8,26 +9,32 @@ interface IExampleForm {
 }
 
 export default function FormPage() {
+  const defaultValues: IExampleForm = useMemo(() => {
+    return { example: "default value", exampleRequired: "" };
+  }, []);
+
   const {
     register,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<IExampleForm>({ defaultValues: { example: "default value" } });
+  } = useForm<IExampleForm>({
+    defaultValues,
+  });
 
   const onSubmit = (data: IExampleForm) => console.log(data);
-
-  console.log(watch("example")); // watch input value by passing the name of it
+  const { example, exampleRequired } = watch();
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex max-w-screen-sm flex-col justify-start">
-        <Input border label="Example Input" {...register("example")} />
+        <Input border label="Example Input" {...register("example")} value={example} />
         <Input
           label="Example Required Input"
           border
           errorText={errors.exampleRequired ? "This field is required" : ""}
           {...register("exampleRequired", { required: true })}
+          value={exampleRequired}
         />
         <Button type="submit" text="Submit" />
       </div>
